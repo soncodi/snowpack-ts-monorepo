32199c68a09966c67509c043e4ba5ff1b9571338
Alex Soncodi <asoncodi@gmail.com>
2021-02-28 22:16:33 -0600

    Bump dep. Fix routes.

--- snowpack.config.js
+++ snowpack.config.js
@@ -2,11 +2,11 @@ const cfg = require('./client/config.json');
 
 module.exports = {
   workspaceRoot: './',
+  routes: [
+    { match: 'routes', src: '.*', dest: '/index.html' }
+  ],
   devOptions: {
     port: cfg.clientPort,
-    routes: [
-      { match: 'routes', src: '.*', dest: '/index.html' }
-    ],
     open: 'none',
     hmr: true,
     hmrErrorOverlay: false,
@@ -19,7 +19,6 @@ module.exports = {
     sourcemap: false
   },
   mount: {
-    './common/src': { url: '/dist/@soncodi/common' },
     './client/src': { url: '/dist' },
     './client/public': { url: '/', static: true }
   }
